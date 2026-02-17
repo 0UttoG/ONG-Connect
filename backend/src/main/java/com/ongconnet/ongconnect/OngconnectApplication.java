a565012af5e1ded8c2a44c316213acd0e91dac1d
Nestor Uto <nestoruto@yahoo.com>
2026-02-17 12:34:52 -0600

    Estructura limpia: Backend y Frontend

---
+++ backend/src/main/java/com/ongconnet/ongconnect/OngconnectApplication.java
@@ -0,0 +1,13 @@
+package com.ongconnet.ongconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OngconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OngconnectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ongconnet/ongconnect/OngconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ongconnet.ongconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OngconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
